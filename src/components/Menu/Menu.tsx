2389720d532f0789f9d173e4b0901bb91e9ccf75
Igor <igordonatti.id@gmail.com>
2023-10-09 15:58:10 -0400

    navigatorComponent concluido

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -9,34 +9,34 @@ const classIcon = 'text-white cursor-pointer mt-1';
 const itemsMenu = [
   {
     description: 'Início',
-    component: <HomeIcon className={classIcon} fontSize='large' />
+    component: <HomeIcon className={classIcon} fontSize='medium' />
   },
   {
     description: 'Serviços',
-    component: <PlaylistAddCheckIcon className={classIcon}  fontSize='large' />
+    component: <PlaylistAddCheckIcon className={classIcon}  fontSize='medium' />
   },
   {
     description: 'Contratar Plano',
-    component: <ShoppingCartIcon className={classIcon}  fontSize='large' />
+    component: <ShoppingCartIcon className={classIcon}  fontSize='medium' />
   }, 
   {
     description: 'Histórico de Planos',
-    component: <RestoreIcon className={classIcon}  fontSize='large' />
+    component: <RestoreIcon className={classIcon}  fontSize='medium' />
   }
 ]
 
 const Menu = () => {
   return (
     <div className="self-center bg-green-800 h-[95%] ml-8 w-[320px] rounded-tl-lg rounded-bl-lg flex flex-col items-center">
-      <AccountCircleIcon className='text-white mt-2 mb-2' fontSize='large' />
-      <span className='text-2xl text-white mb-2 font-poppins'>Menu</span>
+      <AccountCircleIcon className='text-white mt-2 mb-2]' fontSize='large' />
+      <span className='text-lg text-white mb-2 font-poppins'>Menu</span>
       <div className='h-[2px] w-11/12 border-solid border-white bg-white rounded'></div>
       <span className='mt-2'></span>
       {
         itemsMenu.map((icon, index) => (
-          <div key={index} className='flex w-full items-center align-middle content-center p-2 ml-1'>
+          <div key={index} className='flex w-full items-center content-center p-2 ml-1'>
             {icon.component}
-            <span className='text-white text-2xl cursor-pointer ml-2 font-poppins mt-[6px]'>{icon.description}</span>
+            <span className='text-white text-base cursor-pointer ml-2 font-poppins mt-[4px]'>{icon.description}</span>
           </div>
         ))
       }
--- src/pages/Services Page/Services.tsx
+++ src/pages/Services Page/Services.tsx
@@ -1,12 +1,14 @@
 import Menu from "../../components/Menu/Menu"
+import Navigator from "../../components/Navigator/Navigator"
 
 const Services = () => {
   return (
     <div className="w-screen h-screen flex">
       <Menu />
-      <div className="h-[95%] self-center ml-8">
+      <div className="h-[95%] self-center ml-8 w-full mr-6">
         <span className="text-4xl text-green-800 font-poppins font-semibold">Serviços</span>
-      </div>
+        <Navigator />
+     </div>
     </div>
   )
 }
